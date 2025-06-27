2e50f892e020115b46f58454b059891d1b917732
Eriel Cabrera <erielcabrera1234@gmail.com>
2025-06-27 11:01:31 -0600

    added more configs

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,13 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader=" "
+
+-- Line Number config
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.wrap = true
+vim.opt.colorcolumn = "80"
+vim.opt.cursorline = true
